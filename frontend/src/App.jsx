fcff15b7c363054b37a4fabb294aa2b706a953ce
Rahul Lingwal <rahullingwal200@gmail.com>
2026-09-04 23:21:05 +0530

    feat: implement main App component with tab navigation and global base styles

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,26 @@
-function App() {
+import React, { useState } from "react";
+import HomePage from "./pages/HomePage";
+import CommunitiesPage from "./pages/CommunitiesPage";
+import NotesPage from "./pages/NotesPage";
 
-  return (
-    <>
-      <h1 className="bg-red-700 text-white p-2">Vite + React</h1>
-    </>
-  )
+function App() {
+	const [activeTab, setActiveTab] = useState("Notes");
+
+	const handleNavigateTab = (tab) => {
+		setActiveTab(tab);
+	};
+
+	if (activeTab === "Communities") {
+		return <CommunitiesPage onNavigateTab={handleNavigateTab} />;
+	}
+
+	if (activeTab === "Notes") {
+		return <NotesPage onNavigateTab={handleNavigateTab} />;
+	}
+
+	return <HomePage onNavigateTab={handleNavigateTab} />;
 }
 
-export default App
+export default App;
+
+
